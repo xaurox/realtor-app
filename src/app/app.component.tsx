090cf1c522e8c61d65bf043b60282e05107e6557
xaurox <xaurox.sd@gmail.com>
2023-07-06 13:36:28 +0300

    app-structure-1: install react-router-dom@6, add routing schema in app.component

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,7 +1,27 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+
 import "./app.module.scss";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/">
+        {/* Routes available for non-authorized users */}
+        <Route path="offers" />
+        <Route path="offer/:offerId" />
+        <Route path="login" />
+        <Route path="not-found" />
+
+        {/* Routes available to authorize users */}
+        <Route path="account" />
+        <Route path="create-offer" />
+
+        {/* Redirects */}
+        <Route path="" element={<Navigate to="/offers" replace />} />
+        <Route path="*" element={<Navigate to="/not-found" replace />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
