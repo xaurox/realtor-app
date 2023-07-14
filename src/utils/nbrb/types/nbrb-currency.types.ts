0fd6032feb7d454cfcfc70ed8a8188c19a001b59
xaurox <xaurox.sd@gmail.com>
2023-07-14 13:29:30 +0300

    nbrb-api-1: add currency and rate types

---
+++ src/utils/nbrb/types/nbrb-currency.types.ts
@@ -0,0 +1,19 @@
+export interface CurrencyType {
+  Cur_ID: number;
+  Cur_ParentID: number;
+  Cur_Code: string;
+  Cur_Abbreviation: string;
+  Cur_Name: string;
+  Cur_Name_Bel: string;
+  Cur_Name_Eng: string;
+  Cur_QuotName: string;
+  Cur_QuotName_Bel: string;
+  Cur_QuotName_Eng: string;
+  Cur_NameMulti: string;
+  Cur_Name_BelMulti: string;
+  Cur_Name_EngMulti: string;
+  Cur_Scale: number;
+  Cur_Periodicity: number;
+  Cur_DateStart: string;
+  Cur_DateEnd: string;
+}
---
+++ src/utils/nbrb/types/nbrb-rate.types.ts
@@ -0,0 +1,8 @@
+export interface RateType {
+  Cur_ID: number;
+  Date: string;
+  Cur_Abbreviation: string;
+  Cur_Scale: number;
+  Cur_Name: string;
+  Cur_OfficialRate: number;
+}
